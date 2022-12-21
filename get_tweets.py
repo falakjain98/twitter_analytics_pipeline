ac7e5c652892aefe1bea1201c6a52e4d14da6d50
falakjain <jain.falak@hotmail.com>
2022-12-21 10:18:20 -0800

    Save tweets as parquet

--- get_tweets.py
+++ get_tweets.py
@@ -4,22 +4,29 @@ import pandas as pd
 import numpy as np
 
 client = tweepy.Client(bearer_token = config.bearer_token)
+
+# define query
 query = 'FIFA2022 -is:retweet'
 
-start_time = '2022-12-18T17:30:00Z'
-end_time = '2022-12-18T18:30:00Z'
+# start time and end time
+start_time = '2022-12-18T00:00:00Z'
+end_time = '2022-12-19T00:00:00Z'
 
+# output fields
 fields = ['id','tweet','date','source','likes','RTs','lang']
 df = pd.DataFrame(columns = fields)
 
+# run paginated search to extract all tweet
 for tweet in tweepy.Paginator(
             client.search_recent_tweets, query=query,max_results = 10,
             start_time = start_time, end_time = end_time, 
-            tweet_fields = ['created_at','lang','public_metrics']).flatten(limit = 10):
-    if tweet.lang == 'en' or tweet.lang == 'es':
+            tweet_fields = ['created_at','lang','public_metrics']).flatten(limit=10):
+    # only parse english tweets
+    if tweet.lang == 'en':
         df = df.append(
             pd.DataFrame([[tweet.id,tweet.text,tweet.created_at,tweet.source,
                         tweet.public_metrics['like_count'],tweet.public_metrics['retweet_count'],tweet.lang]],
                         columns = fields))
-
-print(df.head())
+    
+# write to parquet gzipped file
+df.to_parquet('api_data/api_data.parquet.gzip',compression='gzip')
